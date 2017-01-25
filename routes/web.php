e113f068d6a2e876b4ec8e6d87b8083147a6f5ce
Richard J Mora <richard.mora@outlook.com>
2017-01-25 16:47:10 -0400

    Account management creation
    Creation of the controller, route and template.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => 'auth'], function(){
+	Route::get('/account', 'UserController@account');
+	Route::post('/account/update/{id}', 'UserController@updateAccount');
+});
